1fe94426a14e5ebf009d3e71486e4e2fa3707ae6
JuanCaycho11 <juancaycho11@gmail.com>
2019-10-05 13:11:42 -0500

    details

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-  URL: 'http://localhost:4000/api/'
+  URL: 'http://localhost:3000/api/'
   // URL: 'http://192.168.2.122:3000/api/'
   // URL: 'https://projectm-vue.herokuapp.com/api/'
 }
