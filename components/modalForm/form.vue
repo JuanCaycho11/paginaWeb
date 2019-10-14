0b9d98a24e6f4630e700d54b964c466b3ea542ce
JuanCaycho11 <juancaycho11@gmail.com>
2019-10-14 13:38:12 -0500

    update-error

--- components/modalForm/form.vue
+++ components/modalForm/form.vue
@@ -28,7 +28,7 @@
         <v-divider />
         <v-card-actions v-if="mostrar">
           <v-spacer />
-          <v-btn color="primary" flat @click="close">
+          <v-btn color="primary" text @click="close">
             Cancelar
           </v-btn>
           <v-btn color="primary" :loading="loading" :disabled="loading" @click="save">
